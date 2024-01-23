91955141dbd8a2eda67373754b7945c073149699
w1nsun <ilya.nevmyvaka@gmail.com>
2024-01-23 17:56:36 +0100

    init

---
+++ src/main/java/com/hyper/gaming/GamingApplication.java
@@ -0,0 +1,13 @@
+package com.hyper.gaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyper/gaming/GamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyper.gaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
